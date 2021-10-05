be6f929e90e19d2545c9b1631d9bc8833117c513
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-05 13:46:46 +0200

    linked FilmCard component (test axios call)

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,13 +1,28 @@
 <template>
-    <div></div>
+    <ul>
+        <li>Title : {{api.title}}</li>
+        <li>Original title : {{api.original_title}}</li>
+        <li>Language : {{api.original_language}}</li>
+        <li>Average rating : {{api.vote_average}}</li>
+    </ul>
 </template>
 
 <script>
 export default {
-    name : "FilmCard"
+    name : "FilmCard",
+    props : {
+        api : Object
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+    ul {
+        display: flex;
+        li {
+            border: .0625rem solid grey;
+            margin: 1em 1em;
+        }
+    }
 
 </style>
--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-        <FilmCard/>
-    </div>
+    <ul>
+        <li v-for="(movie, index) in movies" :key='index'>
+            <FilmCard :api="movie"/>
+        </li>
+    </ul>
 </template>
 
 <script>
-import FilmCard from './FilmCard.vue'
+import FilmCard from './FilmCard.vue';
+import axios from 'axios';
+
 export default {
     name : "Films",
     props : {
@@ -13,6 +17,18 @@ export default {
     },
     components : {
         FilmCard
+    },
+    data() {
+        return {
+            movies: []
+        }
+    },
+    created() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=72cd08f1aa2d4c12d81158ac764c8449&query=ritorno+al+futuro')
+        .then( (response) => {
+            this.movies = response.data.results;
+        }
+        );
     }
 }
 </script>

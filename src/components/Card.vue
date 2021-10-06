a52d6a944f6c783cc88863167d635bf4331fe653
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-06 10:18:14 +0200

    improved flag method

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,7 +18,7 @@ export default {
     },
     methods : {
         flag(isoCode) {
-            if ( this.languages.includes(isoCode) ) {
+            if ( !this.missingLanguages.includes(isoCode) ) {
                 return `https://www.unknown.nu/flags/images/${isoCode}-100`;
             } else {
                 return "https://upload.wikimedia.org/wikipedia/commons/2/2f/Missing_flag.png";
@@ -27,7 +27,7 @@ export default {
     },
     data() {
         return {
-            languages : ['ab','aa','af','ak','sq','am','ar','an','hy','as','av','ae','ay','az','bm','ba','eu','be','bn','bh','bi','bs','br','bg','my','ca','km','ch','ce','ny','zh','cu','cv','kw','co','cr','hr','cs','da','dv','nl','dz','en','eo','et','ee','fo','fj','fi','fr','ff','gd','gl','lg','ka','de','el','kl','gn','ht','ha','he','hz','hi','ho','hu','is','io','ig','id','ia','ie','ga','it','ja','kn','kr','ks','kk','rw','kv','ko','ku','ky','lo','la','lv','lb','li','lt','lu','mk','mg','ms','mt','gv','mi','mh','ro','mn','na','nv','ne','se','no','oc','oj','or','om','os','pa','fa','pl','pt','qu','rm','rn','ru','sm','sg','sc','sr','sn','sd','si','sk','sl','so','st','es','su','ss','sv','ty','tg','ta','tt','th','bo','ti','to','ts','tn','tr','tk','ug','uk','ur','uz','ve','vi','vo','wa','cy','fy','wo','xh','yi','yo','za','zu']
+            missingLanguages : ['gu', 'ii', 'ik', 'iu', 'jv', 'kg', 'ki', 'kj', 'ml', 'mr', 'nb', 'nd', 'ng', 'nn', 'nr', 'pi', 'ps', 'sa', 'sw', 'te', 'tl', 'tw']
         }
     }
 }

d6eb0e38b26fbc8c79f5df8941a551b535d9bb90
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-09 11:53:04 +0200

    trending sections displayed on reset button click

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -3,7 +3,15 @@
         <!-- displayed only if search returns no results -->
         <h1 v-if="noMovieFound == true">NO MOVIES FOUND</h1>
 
-        <!-- displayed only if search returns at least 1 result -->
+        <!-- Trending movies displayed only if search returns at least 1 result -->
+        <h3 v-if="trendingMovies != 0">TRENDING MOVIES</h3>
+        <ul class="row row-cols-xxl-5 container mx-auto">
+            <li class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl" v-for="(movie, movieIndex) in trendingMovies" :key='movieIndex'>
+                <!-- passes api props onto Card component -->
+                <Card :api="movie"/>
+            </li>
+        </ul>
+        <!-- Searched movies displayed only if search returns at least 1 result -->
         <h3 v-if="movies != 0">MOVIES</h3>
         <ul class="row row-cols-xxl-5 container mx-auto">
             <li class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl" v-for="(movie, movieIndex) in movies" :key='movieIndex'>
@@ -30,11 +38,12 @@ export default {
     data() {
         return {
             movies: [],
+            trendingMovies: [],
             noMovieFound : false
         }
     },
-    created(){
-        // Api call for initial display
+    created() {
+        // Api call for initial display of trending movies
         axios
             .get("https://api.themoviedb.org/3/trending/movie/week",{
                 params:{
@@ -44,21 +53,34 @@ export default {
             )
             .then(
                 (response)=>{
-                        this.movies = response.data.results;
+                        this.trendingMovies = response.data.results;
                     }
-            )
+            );
     },
     watch : {
         // listen to changes to prop received (user search value) 
         passedInput() {
-            // avoiding "NO MOVIES FOUND" message to be displayed before API call
+            // avoiding "NO MOVIES FOUND" message and trending movies to be displayed before API call
             this.noMovieFound = false;
+            this.trendingMovies = [];
 
-            // if search string is '' or reset button is clicked, array is emptied
+            // if search string is '' or reset button is clicked, movies array is emptied and trending movies are shown
             if ( this.passedInput == '') {
                     this.movies = [];
+                    axios
+                    .get("https://api.themoviedb.org/3/trending/movie/week",{
+                        params:{
+                            api_key: '72cd08f1aa2d4c12d81158ac764c8449'
+                        }
+                    }
+                    )
+                    .then(
+                        (response)=>{
+                                this.trendingMovies = response.data.results;
+                            }
+                    );
             } else {
-                // API call for movies
+                // API call for movies based on user search input
                 axios.get('https://api.themoviedb.org/3/search/movie', {
                     params : {
                         api_key : '72cd08f1aa2d4c12d81158ac764c8449',
--- src/components/Series.vue
+++ src/components/Series.vue
@@ -3,7 +3,16 @@
         <!-- displayed only if search returns no results -->
         <h1 v-if="noTVFound == true">NO TV SERIES FOUND</h1>
 
-        <!-- displayed only if search returns at least 1 result -->
+        <!-- Trending TV series displayed only if search returns at least 1 result -->
+        <h3 v-if="trendingTV != 0">TRENDING TV SERIES</h3>
+        <ul class="row row-cols-xxl-5 container mx-auto">
+            <li class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl" v-for="(TV, TVindex) in trendingTV" :key='TVindex'>
+                <!-- passes api props onto Card component -->
+                <Card :api="TV"/>
+            </li>
+        </ul>
+
+        <!-- Searched TV series displayed only if search returns at least 1 result -->
         <h3 v-if="TVSeries != 0">TV SERIES</h3>
         <ul class="row row-cols-xxl-5 container mx-auto">
             <li class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl" v-for="(TV, TVindex) in TVSeries" :key='TVindex'>
@@ -30,11 +39,12 @@ export default {
     data() {
         return {
             TVSeries: [],
+            trendingTV: [],
             noTVFound : false
         }
     },
     created(){
-        // Api call for initial display
+        // Api call for initial display of trending TV series
         axios
             .get("https://api.themoviedb.org/3/trending/tv/week",{
                 params:{
@@ -44,21 +54,33 @@ export default {
             )
             .then(
                 (response)=>{
-                        this.TVSeries = response.data.results;
+                        this.trendingTV = response.data.results;
                     }
             )
     },
     watch : {
         // listen to changes to prop received (user search value) 
         passedInput() {
-            // avoiding "NO TV SERIES FOUND" message to be displayed before API call
+            // avoiding "NO TV SERIES FOUND" message and trending series to be displayed before API call
             this.noTVFound = false;
-
-            // if search string is '' or reset button is clicked, array is emptied
+            this.trendingTV = [];
+            // if search string is '' or reset button is clicked,  series array is emptied and trending tvs series are shown
             if ( this.passedInput == '') {
                     this.TVSeries = [];
+                    axios
+                    .get("https://api.themoviedb.org/3/trending/tv/week",{
+                        params:{
+                            api_key: '72cd08f1aa2d4c12d81158ac764c8449'
+                        }
+                    }
+                    )
+                    .then(
+                        (response)=>{
+                                this.trendingTV = response.data.results;
+                            }
+                    )
             } else {
-                // API call for movies
+                // API call for TV series based on user search input
                 axios.get('https://api.themoviedb.org/3/search/tv', {
                     params : {
                         api_key : '72cd08f1aa2d4c12d81158ac764c8449',
@@ -66,7 +88,7 @@ export default {
                         language : 'it-IT'
                     }
                 })
-                // array is populated with movie objects, if 0 "NO TV SERIES FOUND" message is displayed
+                // array is populated with TV series objects, if 0 "NO TV SERIES FOUND" message is displayed
                 .then( (response) => {
                     this.TVSeries = response.data.results;
                     if ( this.TVSeries.length == 0) {

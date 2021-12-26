69e2dd13d8ac09827d5aeec770838e5947838851
elmurie <elmurie@gmail.com>
2021-12-26 18:48:48 +0100

    Revert "modified landing page"
    This reverts commit 6badec5de8d2b78e81309fb8c1060a72f2ae0bec.

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 @echo off
-npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/elmurie/vue-boolflix.git main:gh-pages&&cd ..
+npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/bagus123/vue-gh-pages.git master:gh-pages&&cd ..
